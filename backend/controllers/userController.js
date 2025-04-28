2b2ac2130d420925134cd63550da754365ec4628
sanya kesharwani <133665115+ensanya@users.noreply.github.com>
2025-04-28 13:41:54 +0530

    commit all

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import validator from 'validator';
-import bcrypt from 'bcrypt';
-// import bcrypt from 'bcryptjs';
+// import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import userModel from '../models/userModel.js';
 import jwt from 'jsonwebtoken';
 import { v2 as cloudinary } from 'cloudinary';
